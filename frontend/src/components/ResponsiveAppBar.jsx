1955c9d8f8214cdb576c3da21889aa9fc4697c48
Jerald Wilks <jerald@wilks.pro>
2023-05-15 17:51:00 -0600

    Updated text

--- frontend/src/components/ResponsiveAppBar.jsx
+++ frontend/src/components/ResponsiveAppBar.jsx
@@ -111,7 +111,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            InDeygo
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
