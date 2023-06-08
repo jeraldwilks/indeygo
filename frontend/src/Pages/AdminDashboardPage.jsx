b35c664bb8e31425649ab866aa61d004defaf5de
Jerald Wilks <jerald@wilks.pro>
2023-06-07 21:50:07 -0600

    Remove localforage & fix bad link

--- frontend/src/Pages/AdminDashboardPage.jsx
+++ frontend/src/Pages/AdminDashboardPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AdminDrawer from "../components/AdminDrawer";
+
 import AdminProductType from "../components/AdminProductType";
 
 const AdminDashboardPage = () => {
