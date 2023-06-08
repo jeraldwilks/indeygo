e3ca02656425acaba146f9da188e10bdbf4a3571
FELMONON <FELMONON@GMAIL.COM>
2023-06-08 17:57:14 -0600

    created a fundraisermodel.js  & fundraiserRouter.js

--- backend/routes/fundraiserRouter.js
+++ backend/routes/fundraiserRouter.js
@@ -1,7 +1,38 @@
-import express from "express";
+const express = require('express');
+const router = express.Router();
+const Fundraiser = require('../models/fundraiserModel');
 
-export const fundraiserRouter = express.Router();
+// Create a new fundraiser
+router.post('/', async (req, res) => {
+  try {
+    // Extract the form data from the request body
+    const {
+      organizationName,
+      deliveryAddress,
+      startDate,
+      endDate,
+      fundraiserAmount,
+      expectedProfit
+    } = req.body;
 
-fundraiserRouter.get("/", (req, res) => {
-  res.send("Hi");
+    // Create a new fundraiser object
+    const newFundraiser = new Fundraiser({
+      organizationName,
+      deliveryAddress,
+      startDate,
+      endDate,
+      fundraiserAmount,
+      expectedProfit
+    });
+
+    // Save the fundraiser to the database
+    const savedFundraiser = await newFundraiser.save();
+
+    res.status(201).json(savedFundraiser);
+  } catch (error) {
+    console.error('Error creating fundraiser:', error);
+    res.status(500).json({ error: 'Failed to create fundraiser' });
+  }
 });
+
+module.exports = router;
