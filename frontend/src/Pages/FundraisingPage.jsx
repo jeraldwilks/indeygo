6dded9e2a323eec81e87eac361e21a0bb4bdec89
Jerald Wilks <jerald@wilks.pro>
2023-06-09 00:29:31 -0600

    removed hardcoding of localhost

--- frontend/src/Pages/FundraisingPage.jsx
+++ frontend/src/Pages/FundraisingPage.jsx
@@ -15,7 +15,7 @@ import { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useAuth } from "../providers/AuthProvider";
 import { useNavigate } from "react-router-dom";
-import CookieIcon from '@mui/icons-material/Cookie';
+import CookieIcon from "@mui/icons-material/Cookie";
 const handleSubmit = async (event) => {
   event.preventDefault();
   const data = new FormData(event.currentTarget);
@@ -29,7 +29,7 @@ const handleSubmit = async (event) => {
   const deliveryDate = data.get("deliveryDate");
 
   try {
-    const response = await fetch("http://localhost:4001/api/fundraiser", {
+    const response = await fetch("/api/fundraiser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -48,7 +48,7 @@ const handleSubmit = async (event) => {
 
     if (response.ok) {
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       alert("Successfully Registered!");
       navigate("/Dashboard");
     } else {
@@ -60,136 +60,133 @@ const handleSubmit = async (event) => {
   }
 };
 
-
 const theme = createTheme();
 
 export default function FundraisingPage() {
-   {
-  const { register } = useAuth();
-  const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState("");
+  {
+    const { register } = useAuth();
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const startDate = data.get("startDate");
-    const endDate = data.get("endDate");
-    const organizationName = data.get("organizationName");
-    const fundraiserAmount = data.get("fundraiserAmount");
-    const expectedProfit = data.get("expectedProfit");
-    const deliveryAddress = data.get("deliveryAddress");
-    const orderDate = data.get("orderDate");
-    const deliveryDate = data.get("deliveryDate");
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      const startDate = data.get("startDate");
+      const endDate = data.get("endDate");
+      const organizationName = data.get("organizationName");
+      const fundraiserAmount = data.get("fundraiserAmount");
+      const expectedProfit = data.get("expectedProfit");
+      const deliveryAddress = data.get("deliveryAddress");
+      const orderDate = data.get("orderDate");
+      const deliveryDate = data.get("deliveryDate");
 
-   
-    if (success === true) {
-      alert("Successfully Registered!");
-      navigate("/Dashboard");
-    } else {
-      alert(success);
-    }
-  };
+      if (success === true) {
+        alert("Successfully Registered!");
+        navigate("/Dashboard");
+      } else {
+        alert(success);
+      }
+    };
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar  >
-            <CookieIcon/>
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Register an Organzition
-          </Typography>
+    return (
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="startDate"
-                  required
-                  fullWidth
-                  id="startDate"
-                  label="start date"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="endDate"
-                  label="End Date"
-                  name="endDate"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="organizationName"
-                  label="Organization Name"
-                  name="organizationName"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="fundraiserAmount"
-                  label="Fundraiser Amount"
-                  name="fundraiserAmount"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="expectedProfit"
-                  label="Expected Profit"
-                  id="expectedProfit"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="deliveryAddress"
-                  label="Delivery Address"
-                  id="deliveryAddress"
-                />
-                 
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="orderDate"
-                  label="Order Date"
-                  id="orderDate"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="deliveryDate"
-                  label="Delivery Date"
-                  id="deliveryDate"
-                />
-              </Grid>
-              {/* <Grid item xs={12}>
+            <Avatar>
+              <CookieIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Register an Organzition
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="startDate"
+                    required
+                    fullWidth
+                    id="startDate"
+                    label="start date"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="endDate"
+                    label="End Date"
+                    name="endDate"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="organizationName"
+                    label="Organization Name"
+                    name="organizationName"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="fundraiserAmount"
+                    label="Fundraiser Amount"
+                    name="fundraiserAmount"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="expectedProfit"
+                    label="Expected Profit"
+                    id="expectedProfit"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="deliveryAddress"
+                    label="Delivery Address"
+                    id="deliveryAddress"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="orderDate"
+                    label="Order Date"
+                    id="orderDate"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="deliveryDate"
+                    label="Delivery Date"
+                    id="deliveryDate"
+                  />
+                </Grid>
+                {/* <Grid item xs={12}>
                 <FormControlLabel
                   control={
                     <Checkbox required value="allowExtraEmails" color="primary" />
@@ -197,43 +194,38 @@ export default function FundraisingPage() {
                   label="I want to receive Fundraising updates via email."
                 />
               </Grid> */}
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Start Fundraising
-            </Button>
-         
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Start Fundraising
+              </Button>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </ThemeProvider>
+    );
+  }
+
+  function Copyright(props) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="/">
+          Indeygo
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
 }
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="/">
-        Indeygo
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-}
-
-
-
-
