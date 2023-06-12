1d277fc64fb9d9b8c6c323d6c827abaf509db22f
Jerald Wilks <jerald@wilks.pro>
2023-06-12 12:40:40 -0600

    Can populate product type dropdown

--- frontend/src/components/AdminProduct.jsx
+++ frontend/src/components/AdminProduct.jsx
@@ -1,11 +1,20 @@
+import {
+  FormControl,
+  FormLabel,
+  MenuItem,
+  TextField,
+  InputLabel,
+  Select,
+  Button,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 const AdminProduct = () => {
-  const [productType, setProductType] = useState();
-  const [name, setName] = useState();
-  const [description, setDescription] = useState();
-  const [quantityDesc, setQuantityDesc] = useState();
-  const [sellPrice, setSellPrice] = useState();
+  const [productType, setProductType] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [quantityDesc, setQuantityDesc] = useState("");
+  const [sellPrice, setSellPrice] = useState(1);
   const [wholesalePrices, setWholesalePrices] = useState([]);
   const [availableProductTypes, setAvailableProductTypes] = useState([]);
 
@@ -18,11 +27,41 @@ const AdminProduct = () => {
   }, []);
 
   return (
-    <div>
-      {availableProductTypes.map((availableProductType) => (
-        <h1>{availableProductType.name}</h1>
-      ))}
-    </div>
+    <>
+      <FormControl>
+        <FormLabel>Add a New Product</FormLabel>
+        <br />
+        <InputLabel id="ProductType-label">Product Type</InputLabel>
+        <Select
+          labelId="ProductType-label"
+          id="ProductType"
+          value={productType}
+          // label="Product Type"
+          onChange={(e) => {
+            setProductType(e.target.value);
+          }}
+        >
+          {availableProductTypes.map((availableProductType) => (
+            <MenuItem
+              key={availableProductType.name}
+              value={availableProductType._id}
+            >
+              {availableProductType.name}
+            </MenuItem>
+          ))}
+        </Select>
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Product Name"
+          name="name"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <Button onClick={() => console.log(productType)}>Click</Button>
+      </FormControl>
+    </>
   );
 };
 
--- frontend/src/components/AdminProductType.jsx
+++ frontend/src/components/AdminProductType.jsx
@@ -20,8 +20,9 @@ const AdminProductType = () => {
 
   const updateFieldChanged = (index, value) => {
     let newArr = [...priceTierMin];
-    newArr[index] = value;
+    newArr[index] = parseInt(value);
     setPriceTierMin(newArr);
+    console.log(typeof priceTierMin[index]);
   };
 
   const submitForm = async () => {
@@ -41,69 +42,68 @@ const AdminProductType = () => {
   };
 
   return (
-    <div>
-      <FormControl>
-        <FormLabel>Add a New Product Type</FormLabel>
-        <TextField
-          type="text"
-          variant="outlined"
-          label="Product Type Name"
-          name="name"
-          required
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          type="text"
-          variant="outlined"
-          label="Description of Sell Quantity"
-          name="quantityDesc"
-          required
-          onChange={(e) => setQuantityDesc(e.target.value)}
-        />
-        <TextField
-          type="number"
-          variant="outlined"
-          label="Wholesale Case Size"
-          name="caseSize"
-          required
-          onChange={(e) => setCaseSize(e.target.value)}
-        />
-        <FormLabel>Minimum Values for Pricing Tiers:</FormLabel>
-        {priceTierMin.map((tier, index) => (
-          <React.Fragment key={(index, tier)}>
-            {index === 0 ? (
-              <TextField
-                disabled
-                type="number"
-                variant="outlined"
-                label="Tier 1"
-                name="priceTierMin"
-                defaultValue={priceTierMin[index]}
-                onChange={(e) => updateFieldChanged(index, e.target.value)}
-                required
-              />
-            ) : (
-              <TextField
-                type="number"
-                variant="outlined"
-                label={"Tier " + (index + 1)}
-                name="priceTierMin"
-                defaultValue={priceTierMin[index]}
-                onChange={(e) => updateFieldChanged(index, e.target.value)}
-                required
-                helperText="Specify the minimum number of items sold to get this pricing tier"
-              />
-            )}
-            {priceTierMin.length > 1 && index != 0 && (
-              <Button onClick={() => removeTier(index)}>Remove Tier</Button>
-            )}
-          </React.Fragment>
-        ))}
-        <Button onClick={addTier}>Add Tier</Button>
+    <FormControl>
+      <FormLabel>Add a New Product Type</FormLabel>
+      <TextField
+        type="text"
+        variant="outlined"
+        label="Product Type Name"
+        name="name"
+        required
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField
+        type="text"
+        variant="outlined"
+        label="Description of Consumer Sell Quantity"
+        name="quantityDesc"
+        helperText="Example: Each box contains 5 items"
+        required
+        onChange={(e) => setQuantityDesc(e.target.value)}
+      />
+      <TextField
+        type="number"
+        variant="outlined"
+        label="Wholesale Case Size"
+        name="caseSize"
+        required
+        onChange={(e) => setCaseSize(e.target.value)}
+      />
+      <FormLabel>Minimum Values for Pricing Tiers:</FormLabel>
+      {priceTierMin.map((tier, index) => (
+        <React.Fragment key={"PricingTier" + index}>
+          {index === 0 ? (
+            <TextField
+              disabled
+              type="number"
+              variant="outlined"
+              label="Tier 1"
+              name="priceTierMin"
+              defaultValue={priceTierMin[index]}
+              onChange={(e) => updateFieldChanged(index, e.target.value)}
+              required
+            />
+          ) : (
+            <TextField
+              type="number"
+              variant="outlined"
+              label={"Tier " + (index + 1)}
+              name="priceTierMin"
+              defaultValue={priceTierMin[index]}
+              onChange={(e) => updateFieldChanged(index, e.target.value)}
+              required
+              helperText="Specify the minimum number of items sold to get this pricing tier"
+            />
+          )}
+          {priceTierMin.length > 1 && index != 0 && (
+            <Button onClick={() => removeTier(index)}>Remove Tier</Button>
+          )}
+        </React.Fragment>
+      ))}
+      <Button onClick={addTier}>Add Tier</Button>
 
-        <Button onClick={submitForm}>Add Product Type</Button>
-      </FormControl>
-    </div>
+      <Button onClick={submitForm}>Add Product Type</Button>
+    </FormControl>
   );
 };
 
