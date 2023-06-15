620c2454f43a22b8dec9c87fa11f935257230df7
Jerald Wilks <jerald@wilks.pro>
2023-06-15 14:20:41 -0600

    product addition adding

--- frontend/src/components/AdminProduct.jsx
+++ frontend/src/components/AdminProduct.jsx
@@ -15,7 +15,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 
-
 const theme = createTheme();
 
 const AdminProduct = () => {
@@ -33,6 +32,12 @@ const AdminProduct = () => {
     };
     getProductTypes();
   }, []);
+
+  const updateFieldChanged = (index, value) => {
+    let newArr = [...wholesalePrices];
+    newArr[index] = parseFloat(value);
+    setWholesalePrices(newArr);
+  };
 
   const submitForm = async () => {
     const response = await fetch("/api/product/", {
@@ -53,88 +58,90 @@ const AdminProduct = () => {
 
   return (
     <>
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-      <FormControl>
-        <FormLabel>Add a New Product</FormLabel>
-        <br />
-        <InputLabel id="ProductType-label">Product Type</InputLabel>
-        <Select
-          labelId="ProductType-label"
-          id="ProductType"
-          value={productType}
-          // label="Product Type"
-          onChange={(e) => {
-            setProductType(e.target.value);
-          }}
-          required
-        >
-          {availableProductTypes.map((availableProductType) => (
-            <MenuItem
-              key={availableProductType.name}
-              value={availableProductType}
-            >
-              {availableProductType.name}
-            </MenuItem>
-          ))}
-        </Select>
-        <br/>
-        <Grid item xs={12}>
-        <TextField
-          type="text"
-          variant="outlined"
-          label="Product Name"
-          name="name"
-          required
-          onChange={(e) => setName(e.target.value)}
-        />
-        </Grid>
-        <TextField
-          type="text"
-          variant="outlined"
-          label="Product Description"
-          name="description"
-          required
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <TextField
-          type="number"
-          variant="outlined"
-          label="Consumer Price"
-          name="sellPrice"
-          required
-          onChange={(e) => setSellPrice(e.target.value)}
-        />
-        {productType != [] &&
-          productType.priceTierMin.map((priceTier, index) => (
-            <TextField
-              key={"tier" + index}
-              type="number"
-              variant="outlined"
-              label={"Tier " + (index + 1) + " Price"}
-              helperText={
-                index + 1 === productType.priceTierMin.length
-                  ? priceTier + " or more"
-                  : "Up to " +
-                    (productType.priceTierMin[index + 1] - productType.caseSize)
-              }
-            />
-          ))}
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <FormControl>
+              <FormLabel>Add a New Product</FormLabel>
+              <br />
+              <InputLabel id="ProductType-label">Product Type</InputLabel>
+              <Select
+                labelId="ProductType-label"
+                id="ProductType"
+                value={productType}
+                // label="Product Type"
+                onChange={(e) => {
+                  setProductType(e.target.value);
+                }}
+                required
+              >
+                {availableProductTypes.map((availableProductType) => (
+                  <MenuItem
+                    key={availableProductType.name}
+                    value={availableProductType}
+                  >
+                    {availableProductType.name}
+                  </MenuItem>
+                ))}
+              </Select>
+              <br />
+              <Grid item xs={12}>
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  label="Product Name"
+                  name="name"
+                  required
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Grid>
+              <TextField
+                type="text"
+                variant="outlined"
+                label="Product Description"
+                name="description"
+                required
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <TextField
+                type="number"
+                variant="outlined"
+                label="Consumer Price"
+                name="sellPrice"
+                required
+                onChange={(e) => setSellPrice(e.target.value)}
+              />
+              {productType != [] &&
+                productType.priceTierMin.map((priceTier, index) => (
+                  <TextField
+                    key={"tier" + index}
+                    type="number"
+                    variant="outlined"
+                    label={"Tier " + (index + 1) + " Price"}
+                    helperText={
+                      index + 1 === productType.priceTierMin.length
+                        ? priceTier + " or more"
+                        : "Up to " +
+                          (productType.priceTierMin[index + 1] -
+                            productType.caseSize)
+                    }
+                    onChange={(e) => updateFieldChanged(index, e.target.value)}
+                  />
+                ))}
 
-        <Button onClick={submitForm}>Click</Button>
-      </FormControl>
-      </Box>
-    </Container> 
-    </ThemeProvider> 
+              <Button onClick={submitForm}>Add Product</Button>
+            </FormControl>
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
