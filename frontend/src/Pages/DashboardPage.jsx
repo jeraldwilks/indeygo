d62ad0e62cd6b5c42cdc29b2341e6fafc3caa0f0
Jerald Wilks <jerald@wilks.pro>
2023-06-18 08:28:34 -0600

    Can add organization

--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useAuth } from "../providers/AuthProvider";
 import { Navigate } from "react-router-dom";
 import AdminDashboardPage from "./AdminDashboardPage";
+import OrganizationPage from "./OrganizationPage";
 
-const DashboardPage = ({page}) => {
+const DashboardPage = ({ page }) => {
   const { user } = useAuth();
   if (user?.isAdmin) {
     return <AdminDashboardPage page={page} />;
   }
-  return <div>Dashboard Page</div>;
+  return <OrganizationPage />;
 };
 
 export default DashboardPage;
