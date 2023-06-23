9ebe155edc9c52a63194e9f19fa5baecffc8d8bb
hadep275 <hadep275@gmail.com>
2023-06-23 08:57:36 -0600

    phone # auto fills

--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -112,7 +112,7 @@ export default function RegisterPage() {
                   id="phoneNumber"
                   label="Phone Number"
                   name="phoneNumber"
-                  autoComplete="phoneNumber"
+                  autoComplete="tel"
                 />
               </Grid>
               <Grid item xs={12}>
