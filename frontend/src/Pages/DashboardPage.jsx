72cab501ecdd199c7ac16c448451fa48bd096ff9
hadep275 <hadep275@gmail.com>
2023-06-24 19:28:09 -0600

    Sidebar Component Created

--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
 import AdminDashboardPage from "./AdminDashboardPage";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import OrganizationPage from "./OrganizationPage";
+import React, { useState } from "react";
 import { useAuth } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Sidebar, Menu, MenuItem as SidebarMenuItem } from "react-pro-sidebar";
 import {
@@ -16,6 +17,7 @@ import {
 const DashboardPage = ({ page }) => {
   const [collapsed, setCollapsed] = useState(false);
   const [toggled, setToggled] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
 
   const handleToggleSidebar = () => {
     setToggled(!toggled);
@@ -25,48 +27,73 @@ const DashboardPage = ({ page }) => {
     setCollapsed(!collapsed);
   };
 
-
   const { user } = useAuth();
   if (user?.isAdmin) {
     return <AdminDashboardPage page={page} />;
   }
   return (
     <>
-    <div style={{ display: "flex" }}>
-      <Sidebar
-      collapsed={collapsed}
-      toggled={toggled}
-      onToggle={handleToggleSidebar}
-      >
-        <Menu iconShape="circle">
-          {collapsed ? (
-            <SidebarMenuItem
-              icon={<FaAngleDoubleRight />}
-              onClick={handleCollapsedChange}
-            />
-          ) : (
-            <SidebarMenuItem
-              suffix={<FaAngleDoubleLeft />}
-              onClick={handleCollapsedChange}
-            />
+      <div style={{ display: sidebarOpen ? "flex" : "block" }}>
+      {!sidebarOpen && (
+          <div
+            onClick={() => setSidebarOpen(true)}
+            style={{
+              width: "10%",
+              color: "blue",
+              position: "absolute",
+              top: 64,
+            }}
+          >
+            <ArrowDropDownIcon />
+          </div>
+        )}
+        {sidebarOpen && (
+        <Sidebar
+          collapsed={collapsed}
+          toggled={toggled}
+          onToggle={handleToggleSidebar}
+        >
+          <div
+              onClick={() => setSidebarOpen(!sidebarOpen)}
+              style={{ width: "100%", color: "blue" }}
+            >
+              {sidebarOpen && <ArrowDropUpIcon />}
+              {!sidebarOpen && <ArrowDropDownIcon />}
+            </div>
+          {sidebarOpen && (
+              <>
+          <Menu iconShape="circle">
+            {collapsed ? (
+              <SidebarMenuItem
+                icon={<FaAngleDoubleRight />}
+                onClick={handleCollapsedChange}
+              />
+            ) : (
+              <SidebarMenuItem
+                suffix={<FaAngleDoubleLeft />}
+                onClick={handleCollapsedChange}
+              />
+            )}
+            <Link to="/Dashboard">
+              <SidebarMenuItem icon={<FaTachometerAlt />}>
+                Dashboard
+              </SidebarMenuItem>
+            </Link>
+            <Link to="/OrganizationPage">
+              <SidebarMenuItem icon={<FaGem />}>Organization</SidebarMenuItem>
+            </Link>
+            {/* Add more menu items as needed */}
+          </Menu>
+          </>
           )}
-          <Link to="/dashboard">
-            <SidebarMenuItem icon={<FaTachometerAlt />}>
-              Dashboard
-            </SidebarMenuItem>
-          </Link>
-          <Link to="/OrganizationPage">
-            <SidebarMenuItem icon={<FaGem />}>Organization</SidebarMenuItem>
-          </Link>
-          {/* Add more menu items as needed */}
-        </Menu>
-      </Sidebar>
-      <div style={{ flex: 1 }}>{page === "OrganizationPage" && <OrganizationPage />}
-      </div>
+        </Sidebar>
+        )}
+        <div style={{ flex: 1 }}>
+          {page === "OrganizationPage" && <OrganizationPage />}
+        </div>
       </div>
     </>
   );
-  // <OrganizationPage />;
 };
 
 export default DashboardPage;
