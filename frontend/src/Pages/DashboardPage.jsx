ddcbd7a925cb085aecb49bd084182e604d3c2ad4
hadep275 <hadep275@gmail.com>
2023-06-25 00:57:41 -0600

    Sidebar Menu Complete

--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -1,7 +1,9 @@
+import AddIcon from "@mui/icons-material/Add";
 import AdminDashboardPage from "./AdminDashboardPage";
-import DragHandleSharpIcon from '@mui/icons-material/DragHandleSharp';
+import DragHandleSharpIcon from "@mui/icons-material/DragHandleSharp";
 import OrganizationPage from "./OrganizationPage";
 import React, { useState } from "react";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { useAuth } from "../providers/AuthProvider";
 import { Link } from "react-router-dom";
 import { Sidebar, Menu, MenuItem as SidebarMenuItem } from "react-pro-sidebar";
@@ -33,59 +35,82 @@ const DashboardPage = ({ page }) => {
   return (
     <>
       <div style={{ display: sidebarOpen ? "flex" : "block" }}>
-      {!sidebarOpen && (
+        {!sidebarOpen && (
           <div
             onClick={() => setSidebarOpen(true)}
             style={{
-              width: "10%",
-              color: "blue",
+              width: "4.55%",
+              color: "black",
               position: "absolute",
-              top: 64,
+              top: 68,
             }}
           >
             <DragHandleSharpIcon />
           </div>
         )}
         {sidebarOpen && (
-        <Sidebar
-          collapsed={collapsed}
-          toggled={toggled}
-          onToggle={handleToggleSidebar}
-        >
-          <div
+          <Sidebar
+            collapsed={collapsed}
+            toggled={toggled}
+            onToggle={handleToggleSidebar}
+          >
+            <div
               onClick={() => setSidebarOpen(!sidebarOpen)}
-              style={{ width: "100%", color: "blue" }}
+              style={{ width: "100%", color: "black" }}
             >
               {sidebarOpen && <DragHandleSharpIcon />}
               {!sidebarOpen && <DragHandleSharpIcon />}
             </div>
-          {sidebarOpen && (
+            {sidebarOpen && (
               <>
-          <Menu iconShape="circle">
-            {collapsed ? (
-              <SidebarMenuItem
-                icon={<FaAngleDoubleRight />}
-                onClick={handleCollapsedChange}
-              />
-            ) : (
-              <SidebarMenuItem
-                suffix={<FaAngleDoubleLeft />}
-                onClick={handleCollapsedChange}
-              />
+                <Menu iconShape="circle">
+                  {collapsed ? (
+                    <SidebarMenuItem
+                      icon={<FaAngleDoubleRight />}
+                      onClick={handleCollapsedChange}
+                    />
+                  ) : (
+                    <SidebarMenuItem
+                      suffix={<FaAngleDoubleLeft />}
+                      onClick={handleCollapsedChange}
+                    />
+                  )}
+                  <hr></hr>
+                  <Link
+                    to="/Dashboard"
+                    style={{
+                      color: "black",
+                    }}
+                  >
+                    <SidebarMenuItem icon={<FaTachometerAlt />}>
+                      Dashboard
+                    </SidebarMenuItem>
+                  </Link>
+                  <Link
+                    to="/OrganizationPage"
+                    style={{
+                      color: "black",
+                    }}
+                  >
+                    <SidebarMenuItem icon={<AddIcon />}>
+                      Organization
+                    </SidebarMenuItem>
+                  </Link>
+                  <hr></hr>
+                  <Link
+                    to="/Logout"
+                    style={{
+                      color: "black",
+                    }}
+                  >
+                    <SidebarMenuItem icon={<LogoutIcon />}>Logout</SidebarMenuItem>
+                  </Link>
+                  
+                  {/* Add more menu items as needed */}
+                </Menu>
+              </>
             )}
-            <Link to="/Dashboard">
-              <SidebarMenuItem icon={<FaTachometerAlt />}>
-                Dashboard
-              </SidebarMenuItem>
-            </Link>
-            <Link to="/OrganizationPage">
-              <SidebarMenuItem icon={<FaGem />}>Organization</SidebarMenuItem>
-            </Link>
-            {/* Add more menu items as needed */}
-          </Menu>
-          </>
-          )}
-        </Sidebar>
+          </Sidebar>
         )}
         <div style={{ flex: 1 }}>
           {page === "OrganizationPage" && <OrganizationPage />}
--- frontend/src/components/AdminEditProduct.jsx
+++ frontend/src/components/AdminEditProduct.jsx
@@ -83,7 +83,7 @@ const AdminEditProduct = () => {
           <CssBaseline />
           <Box
             sx={{
-              marginTop: 8,
+              marginTop: 2,
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
