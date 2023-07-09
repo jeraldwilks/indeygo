57ed904111155fb0ed1046e6497e8eb936bdbd3e
rinij <rinimani7@gmail.com>
2023-07-09 11:21:28 -0600

    ProductsPagedone

--- frontend/src/Pages/ProductsPage.jsx
+++ frontend/src/Pages/ProductsPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import ProductDisplay from "../components/ProductDisplay";
 
 const ProductsPage = () => {
   return (
   <>
   <div style={{ marginTop: "100px" }}></div>
-  <div>Products page</div>
+  <div>Our Products</div>
+  <br/>
+  <p1>Check out our wide selection of products to choose from! </p1>
+  <br/>
+  <br/>
+  <ProductDisplay></ProductDisplay>
   
   </>
   );
