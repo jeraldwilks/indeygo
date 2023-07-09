155bec2c6012ad4a71ddebae30ea73520626dc42
rinij <rinimani7@gmail.com>
2023-07-09 08:39:26 -0600

    responsiveness to review component

--- frontend/src/components/CustomerReviewCarousel.jsx
+++ frontend/src/components/CustomerReviewCarousel.jsx
@@ -1,5 +1,78 @@
 
 
+// import React from 'react';
+// import { Box, Typography, Grid, Card, CardContent } from '@mui/material';
+
+// const testimonials = [
+//   {
+//     id: 1,
+//     name: 'John Doe',
+//     role: 'School A',
+//     quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+//   },
+//   {
+//     id: 2,
+//     name: 'Jane Smith',
+//     role: 'School B',
+//     quote: 'Praesent sagittis ex id purus lobortis varius.',
+//   },
+//   {
+//     id: 3,
+//     name: 'Amith',
+//     role: 'School B',
+//     quote: 'Praesent sagittis ex id purus lobortis varius.',
+//   },
+//   // Add more testimonials as needed
+// ];
+
+// const carouselStyle = {
+//   display: 'flex',
+//   flexDirection: 'coloumn',
+//   alignItems: 'center',
+//   backgroundColor: '#f2f2f2',
+//   padding: '20px',
+//   marginTop: 10,
+//   width: '100%',
+//   minHeight: "75vh",
+//   fontFamily: 'Roboto, sans-serif',
+// };
+
+// const CustomerReviewCarousel = () => {
+//   return (
+//     <div style={carouselStyle}>
+//       <Box>
+//         <Typography variant="h4" align="center" gutterBottom>
+//           What Our Customers Are Saying
+//         </Typography>
+//         <Grid container spacing={4} justifyContent="center">
+//           {testimonials.map((testimonial) => (
+//             <Grid item key={testimonial.id} xs={12} sm={6} md={4}>
+//               <Card variant="outlined" sx={{ height: '150%' }}>
+//                 <CardContent>
+//                   <Typography variant="h6" gutterBottom>
+//                     {testimonial.name}
+//                   </Typography>
+//                   <Typography variant="subtitle1" gutterBottom>
+//                     {testimonial.role}
+//                   </Typography>
+//                   <Typography variant="body1">{testimonial.quote}</Typography>
+//                 </CardContent>
+//               </Card>
+//             </Grid>
+//           ))}
+//         </Grid>
+//       </Box>
+//     </div>
+//   );
+// };
+
+// export default CustomerReviewCarousel;
+
+
+
+
+
+
 import React from 'react';
 import { Box, Typography, Grid, Card, CardContent } from '@mui/material';
 
@@ -25,29 +98,28 @@ const testimonials = [
   // Add more testimonials as needed
 ];
 
-const carouselStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  backgroundColor: '#f2f2f2',
-  padding: '20px',
-  marginTop: 100,
-  width: '100%',
-  minHeight: "30vh",
-  fontFamily: 'Roboto, sans-serif',
-};
-
 const CustomerReviewCarousel = () => {
   return (
-    <div style={carouselStyle}>
-      <Box>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#f2f2f2',
+        padding: '20px',
+        marginTop: 10,
+        minHeight: '30vh',
+        fontFamily: 'Roboto, sans-serif',
+      }}
+    >
+      <Box sx={{ maxWidth: '1200px', width: '100%', px: 2 }}>
         <Typography variant="h4" align="center" gutterBottom>
           What Our Customers Are Saying
         </Typography>
         <Grid container spacing={4} justifyContent="center">
           {testimonials.map((testimonial) => (
             <Grid item key={testimonial.id} xs={12} sm={6} md={4}>
-              <Card variant="outlined" sx={{ height: '150%' }}>
+              <Card variant="outlined" sx={{ height: '100%' }}>
                 <CardContent>
                   <Typography variant="h6" gutterBottom>
                     {testimonial.name}
@@ -62,7 +134,7 @@ const CustomerReviewCarousel = () => {
           ))}
         </Grid>
       </Box>
-    </div>
+    </Box>
   );
 };
 
