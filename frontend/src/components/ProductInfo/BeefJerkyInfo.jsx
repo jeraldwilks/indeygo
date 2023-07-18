b67e46fbf90391c18d3b4c70318588b791f737db
rinij <rinimani7@gmail.com>
2023-07-18 11:32:12 -0600

    brochure download feature on Beef Jerky fundraiser

--- frontend/src/components/ProductInfo/BeefJerkyInfo.jsx
+++ frontend/src/components/ProductInfo/BeefJerkyInfo.jsx
@@ -5,7 +5,6 @@ import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import StarsIcon from "@mui/icons-material/Stars";
 import { Button } from "@mui/material";
-
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -14,53 +13,63 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+// Custom styles for the button
+const DownloadButton = styled(Button)({
+  border: "2px solid #0B4D83", // Replace #000 with your desired border color
+  padding: "10px 20px",
+  fontSize: "16px",
+  fontWeight: "bold",
+  fontPalette: "#0B4D83",
+  marginTop: "30px",
+});
+
+// download pdf button here
+
 const BeefJerkyInfo = () => {
-
   const handleDownloadPDF = () => {
-    const pdfPath = '../pdfs/BeefJerky2022.pdf';
-    window.open(pdfPath, '_blank');
+    const pdfPath = "../pdfs/BeefJerky2022.pdf";
+    window.open(pdfPath, "_blank");
   };
-
-
 
   return (
     <>
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
+      <div style={{ position: "relative" }}>
+        <img
+          src="../images/productbeefjerky.jpeg"
+          alt="Beef Jerky Image"
+          style={{
+            width: "1000px",
+            height: "250px", // Adjust the height as needed
+            objectFit: "cover",
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            color: "#fff",
+            fontSize: "32px",
+            fontWeight: "bold",
+          }}
+        >
+          Beef Jerky Fundraiser
+        </div>
+      </div>
+
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
+          flexDirection: "row",
           alignItems: "center",
           justifyContent: "center",
-          minHeight: "100vh",
+          // minHeight: "100vh",
         }}
       >
-        <Box sx={{ width: "850px", position: "relative" }}>
-          <div style={{ position: "relative" }}>
-            <img
-              src="../images/productbeefjerky.jpeg"
-              alt="Beef Jerky Image"
-              style={{
-                width: "100%",
-                height: "250px", // Adjust the height as needed
-                objectFit: "cover",
-              }}
-            />
-            <div
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                color: "#fff",
-                fontSize: "32px",
-                fontWeight: "bold",
-              }}
-            >
-              Beef Jerky Fundraiser
-            </div>
-          </div>
-
-          <Stack spacing={2}>
+        <Box sx={{ width: "100%", position: "relative", ml: 30 }}>
+          <Stack spacing={4}>
             <Item>
               <StarsIcon /> Make 30% Profit per bag
             </Item>
@@ -85,8 +94,28 @@ const BeefJerkyInfo = () => {
             </Item>
           </Stack>
         </Box>
-        <Button onClick={handleDownloadPDF}>Download PDF</Button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="../images/BeefJerkyFrontPage.jpeg"
+            alt="Beef Jerky Image"
+            style={{
+              width: "50%",
+              height: "150%", // Adjust the height as needed
+              objectFit: "cover",
+            }}
+          />
+          <DownloadButton onClick={handleDownloadPDF}>
+            Download Brochure
+          </DownloadButton>
+        </div>
       </div>
+      </Box>
     </>
   );
 };
